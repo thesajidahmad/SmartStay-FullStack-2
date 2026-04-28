4dd221f7328440d37f0b6909e8f5e18d44319bfc
thesajidahmad <101032376+thesajidahmad@users.noreply.github.com>
2026-04-28 15:18:00 +0530

    middleware change

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,12 +1,31 @@
 import User from "../models/User.js";
+import { clerkClient } from "@clerk/express";
 
 // Middleware to check if user is authenticated
 export const protect = async (req, res, next) => {
   const { userId } = req.auth;
   if (!userId) {
-    return res.status(401).json({ success: false, message: "not authenticated" });
+    return res
+      .status(401)
+      .json({ success: false, message: "not authenticated" });
   }
-  const user = await User.findById(userId);
+  let user = await User.findById(userId);
+  if (!user) {
+    try {
+      const clerkUser = await clerkClient.users.getUser(userId);
+      user = await User.create({
+        _id: userId,
+        email: clerkUser.emailAddresses[0].emailAddress,
+        username: `${clerkUser.firstName} ${clerkUser.lastName}`.trim(),
+        image: clerkUser.imageUrl,
+        recentSearchedCities: [],
+      });
+    } catch (e) {
+      return res
+        .status(500)
+        .json({ success: false, message: "Failed to create user" });
+    }
+  }
   req.user = user;
   next();
 };
